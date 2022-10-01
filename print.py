72b6a76a9697ef59d6dd1467026e9a3be8326e78
Yannick Djomo <67871275+franckya@users.noreply.github.com>
2022-10-01 11:50:42 -0400

    removing contents

--- print.py
+++ print.py
@@ -1,8 +1,1 @@
-#Input
-game_first_name = input("What is your first name? ")
-game_last_name = input("What is your last name? ")
-game_age = input("How old are you? ")
 
-print("Your first name is " + game_first_name)
-print("Your last name is " + game_last_name)
-print("You are currently " + game_age + " years old!")
